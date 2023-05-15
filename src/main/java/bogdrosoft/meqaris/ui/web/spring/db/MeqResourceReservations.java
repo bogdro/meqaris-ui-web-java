049cdf21f04e51798139de8b8b00279c3672dc85
Bogdan Drozdowski <>
2023-05-15 22:25:01 +0200

    Update MeqResourceReservations to new db version

--- src/main/java/bogdrosoft/meqaris/ui/web/spring/db/MeqResourceReservations.java
+++ src/main/java/bogdrosoft/meqaris/ui/web/spring/db/MeqResourceReservations.java
@@ -1,8 +1,5 @@
 package bogdrosoft.meqaris.ui.web.spring.db;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Map;
 import java.util.Objects;
 
@@ -12,8 +9,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 @Entity
 @Table(name = "meq_resource_reservations")
@@ -31,24 +26,8 @@ public class MeqResourceReservations {
 	@Column(name = "rr_interval", nullable = false, insertable = false, updatable = false)
 	private Object interval;
 
-	@Column(name = "rr_organiser", nullable = false, insertable = false, updatable = false)
-	private String organiser;
-
-	@Column(name = "rr_summary", insertable = false, updatable = false)
-	private String summary;
-
-	@Temporal(TemporalType.TIMESTAMP)
-	@Column(name = "rr_dtstamp", insertable = false, updatable = false)
-	private Calendar dateTimestamp;
-
-	@Column(name = "rr_uid", nullable = false, insertable = false, updatable = false)
-	private String uid;
-
-	@Column(name = "rr_seq", nullable = false, insertable = false, updatable = false)
-	private Integer seq;
-
-	@Column(name = "rr_data", insertable = false, updatable = false)
-	private String data;
+	@Column(name = "rr_e_id", nullable = false, insertable = false, updatable = false)
+	private Long eventId;
 
 	public Long getId() {
 		return id;
@@ -74,57 +53,17 @@ public class MeqResourceReservations {
 		this.interval = interval;
 	}
 
-	public String getOrganiser() {
-		return organiser;
+	public Long getEventId() {
+		return eventId;
 	}
 
-	public void setOrganiser(String organiser) {
-		this.organiser = organiser;
-	}
-
-	public String getSummary() {
-		return summary;
-	}
-
-	public void setSummary(String summary) {
-		this.summary = summary;
-	}
-
-	public Calendar getDateTimestamp() {
-		return dateTimestamp;
-	}
-
-	public void setDateTimestamp(Calendar dateTimestamp) {
-		this.dateTimestamp = dateTimestamp;
-	}
-
-	public String getUid() {
-		return uid;
-	}
-
-	public void setUid(String uid) {
-		this.uid = uid;
-	}
-
-	public Integer getSeq() {
-		return seq;
-	}
-
-	public void setSeq(Integer seq) {
-		this.seq = seq;
-	}
-
-	public String getData() {
-		return data;
-	}
-
-	public void setData(String data) {
-		this.data = data;
+	public void setEventId(Long eventId) {
+		this.eventId = eventId;
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(data, dateTimestamp, id, interval, organiser, resourceId, seq, summary, uid);
+		return Objects.hash(eventId, id, interval, resourceId);
 	}
 
 	@Override
@@ -136,18 +75,14 @@ public class MeqResourceReservations {
 			return false;
 		}
 		MeqResourceReservations other = (MeqResourceReservations) obj;
-		return Objects.equals(data, other.data) && Objects.equals(dateTimestamp, other.dateTimestamp)
-				&& Objects.equals(id, other.id) && Objects.equals(interval, other.interval)
-				&& Objects.equals(organiser, other.organiser) && Objects.equals(resourceId, other.resourceId)
-				&& Objects.equals(seq, other.seq) && Objects.equals(summary, other.summary)
-				&& Objects.equals(uid, other.uid);
+		return Objects.equals(eventId, other.eventId) && Objects.equals(id, other.id)
+				&& Objects.equals(interval, other.interval) && Objects.equals(resourceId, other.resourceId);
 	}
 
 	@Override
 	public String toString() {
 		return "MeqResourceReservations [id=" + id + ", resourceId=" + resourceId + ", interval=" + interval
-				+ ", organiser=" + organiser + ", summary=" + summary + ", dateTimestamp=" + dateTimestamp + ", uid="
-				+ uid + ", seq=" + seq + ", data=" + data + "]";
+				+ ", eventId=" + eventId + "]";
 	}
 
 	public static MeqResourceReservations buildFromMap(Map<String, Object> m) {
@@ -165,37 +100,9 @@ public class MeqResourceReservations {
 			if (interval != null) {
 				rr.setInterval(interval);
 			}
-			Object organiser = m.get("rr_organiser");
-			if (organiser != null) {
-				rr.setOrganiser(String.valueOf(organiser));
-			}
-			Object summary = m.get("rr_summary");
-			if (summary != null) {
-				rr.setSummary(String.valueOf(summary));
-			}
-			Object dtstamp = m.get("rr_dtstamp");
-			if (dtstamp != null) {
-				// e.g. 20220428T220223Z
-				SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd'T'HHmmss'Z'X");
-				Calendar c = Calendar.getInstance();
-				try {
-					c.setTime(sdf.parse(String.valueOf(dtstamp)));
-				} catch (ParseException e) {
-					c.setTimeInMillis(0);
-				}
-				rr.setDateTimestamp(c);
-			}
-			Object uid = m.get("rr_uid");
-			if (uid != null) {
-				rr.setUid(String.valueOf(uid));
-			}
-			Object seq = m.get("rr_seq");
-			if (seq != null) {
-				rr.setSeq(Integer.valueOf(String.valueOf(seq)));
-			}
-			Object data = m.get("rr_data");
-			if (data != null) {
-				rr.setData(String.valueOf(data));
+			Object evId = m.get("rr_e_id");
+			if (evId != null) {
+				rr.setEventId(Long.valueOf(String.valueOf(evId)));
 			}
 		}
 		return rr;
